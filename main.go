c0fa2aa6588720067fcf961a587b4b8f4f35974b
Chris Kane <github-logs@nein.digital>
2024-08-21 21:44:46 -0500

    add load/save loadGzip/saveGzip functions

--- main.go
+++ main.go
@@ -6,59 +6,18 @@ import (
 	"example.com/pkg/deck"
 )
 
-func War() {
-	// creates a new deck
-	deck := deck.NewDeck()
-
-	// shuffles the deck
-	deck.Shuffle()
-	fmt.Println()
-
-	fmt.Println("Warrrrrrrr!!!!!!")
-
-	fmt.Println()
-
-	// picks a card from the deck
-	// prints "Your Card is {Card}"
-	card, err := deck.PickACard()
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Your Card is", deck.Title((card)))
-	}
-	fmt.Println()
-
-	// shuffles the deck
-	deck.Shuffle()
-	fmt.Println()
-
-	// picks a card from the deck
-	// prints "Your Card is {Card}"
-	card, err = deck.PickACard()
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("My Card is", deck.Title(card))
-	}
-
-	// iterates over teh remaining cards in the deck
-	// prints "Remaining Cards: {Number}"
-	for deck.RemainingCards() > 0 {
-		card, err = deck.PickACard()
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println("My Card", deck.Title(card), deck.IdentifyCard(deck.Title(card)))
-		}
-	}
-}
-
 func main() {
 	// creates a new deck
-	deck := deck.NewDeck()
+	deck1, err := deck.LoadGzip("./deck.bin.gz")
+	if err != nil {
+		fmt.Println("Error Loadig:", err)
+		deck1 = deck.NewDeck()
+	} else {
+		fmt.Println(deck1.RemainingCards())
+	}
 
 	// shuffles the deck
-	deck.Shuffle()
+	deck1.Shuffle()
 	fmt.Println()
 
 	fmt.Println("Warrrrrrrr!!!!!!")
@@ -67,35 +26,40 @@ func main() {
 
 	// picks a card from the deck
 	// prints "Your Card is {Card}"
-	card, err := deck.PickACard()
+	card, err := deck1.PickACard()
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
-		fmt.Println("Your Card is", deck.Title((card)))
+		fmt.Println("Your Card is", deck1.Title((card)))
 	}
 	fmt.Println()
 
 	// shuffles the deck
-	deck.Shuffle()
+	deck1.Shuffle()
 	fmt.Println()
 
 	// picks a card from the deck
 	// prints "Your Card is {Card}"
-	card, err = deck.PickACard()
+	card, err = deck1.PickACard()
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
-		fmt.Println("My Card is", deck.Title(card))
+		fmt.Println("My Card is", deck1.Title(card))
+	}
+
+	_, err = deck1.SaveGzip()
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	// iterates over teh remaining cards in the deck
 	// prints "Remaining Cards: {Number}"
-	for deck.RemainingCards() > 0 {
-		card, err = deck.PickACard()
+	for deck1.RemainingCards() > 0 {
+		card, err = deck1.PickACard()
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println("My Card", deck.Title(card), deck.IdentifyCard(deck.Title(card)))
+			fmt.Println("My Card", deck1.Title(card), deck1.IdentifyCard(deck1.Title(card)))
 		}
 	}
 }
--- pkg/deck/deck.go
+++ pkg/deck/deck.go
@@ -1,9 +1,12 @@
 package deck
 
 import (
+	"compress/gzip"
+	"encoding/gob"
 	"errors"
 	"fmt"
 	"math/rand"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -12,9 +15,9 @@ const CARD_COUNT = 15
 
 // Deck is a deck of cards with field types initialized and assigned to the types of cards
 type Deck struct {
-	cards     []Card
-	faceCards []string
-	types     []string
+	Cards     []Card
+	FaceCards []string
+	Types     []string
 }
 
 // NewDeck creates a new deck of cards
@@ -27,16 +30,16 @@ func NewDeck(types ...string) *Deck {
 	}
 
 	deck := &Deck{
-		cards: make([]Card, 0, len(types)*CARD_COUNT),
-		faceCards: []string{
+		Cards: make([]Card, 0, len(types)*CARD_COUNT),
+		FaceCards: []string{
 			"Jack", "Queen", "King", "Ace", "Joker",
 		},
-		types: types,
+		Types: types,
 	}
 
-	for _, suit := range deck.types {
+	for _, suit := range deck.Types {
 		for j := 1; j <= CARD_COUNT; j++ {
-			deck.cards = append(deck.cards, NewCard(j, suit))
+			deck.Cards = append(deck.Cards, NewCard(j, suit))
 		}
 	}
 	return deck
@@ -52,40 +55,32 @@ func (d *Deck) Title(c *Card) string {
 	if c.ID < 10 {
 		return fmt.Sprintf("%d of %s", c.ID, c.Suit)
 	}
-	return fmt.Sprintf("%s of %s", d.faceCards[(c.ID-1)%len(d.faceCards)], c.Suit)
+	return fmt.Sprintf("%s of %s", d.FaceCards[(c.ID-1)%len(d.FaceCards)], c.Suit)
 }
 
 // Shuffle shuffles the deck using the Fisher-Yates algorithm
 func (d *Deck) Shuffle() {
 	fmt.Println("Shuffling Deck")
 	// Implement Fisher-Yates shuffle
-	for i := len(d.cards) - 1; i > 0; i-- {
+	for i := len(d.Cards) - 1; i > 0; i-- {
 		j := rand.Intn(i + 1)
-		d.cards[i], d.cards[j] = d.cards[j], d.cards[i]
+		d.Cards[i], d.Cards[j] = d.Cards[j], d.Cards[i]
 	}
 }
 
 // RemainingCards returns the number of cards left in the deck
 func (d *Deck) RemainingCards() int {
-	return len(d.cards)
+	return len(d.Cards)
 }
 
 // PickACard picks a card from the deck and returns it as a string
 func (d *Deck) PickACard() (card *Card, err error) {
-	if len(d.cards) == 0 {
+	if len(d.Cards) == 0 {
 		return nil, errors.New("no cards left in the deck")
 	}
-	card = &d.cards[0]
-	d.cards = d.cards[1:] // Remove the card from the deck
+	card = &d.Cards[0]
+	d.Cards = d.Cards[1:] // Remove the card from the deck
 	return card, nil
-}
-
-// Helper Functions
-
-// Parse string for title and ID by splitting string with " of "
-func parseTitle(title string) (string, string) {
-	parts := strings.Split(title, " of ")
-	return parts[0], parts[1]
 }
 
 // IdentifyCard identifies the card by its suit and ID
@@ -111,4 +106,112 @@ func (d *Deck) IdentifyCard(title string) map[string]interface{} {
 		"id":   id,
 		"suit": suit,
 	}
+}
+
+// Save deck state by writing the Deck object to a file
+func (d *Deck) Save() (string, error) {
+	filePath := "./deck.bin" // Replace with the actual file path
+
+	// Open the file for writing
+	file, err := os.Create(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// Create an encoder
+	encoder := gob.NewEncoder(file)
+
+	// Encode and write the deck object to the file
+	err = encoder.Encode(d)
+	if err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
+
+/* Save deck state.
+ * Write the deck object to a file using compress/gzip package
+ * return the file path and an error */
+func (d *Deck) SaveGzip() (string, error) {
+	filePath := "./deck.bin.gz" // Replace with the actual file path
+
+	// Open the file for writing
+	file, err := os.Create(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// Create a gzip writer
+	gzipWriter := gzip.NewWriter(file)
+	defer gzipWriter.Close()
+
+	// Create an encoder
+	encoder := gob.NewEncoder(gzipWriter)
+	// Encode and write the deck object to the file
+	err = encoder.Encode(d)
+	if err != nil {
+		return "", err
+	}
+
+	return filePath, nil
+}
+
+// Load deck state by reading the Deck object from a file
+// return a deck object and an error after decoding
+// with gob.NewDecoder(file)
+func Load(filePath string) (*Deck, error) {
+	// Open the file for reading
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	// Decode and read the deck object from the file
+	var deck Deck
+	err = decoder.Decode(&deck)
+	if err != nil {
+		return nil, err
+	}
+	return &deck, nil
+}
+
+/* Load deck state.
+ * Read the deck object to a file using compress/gzip package
+ * return the deck object and an error */
+func LoadGzip(filePath string) (*Deck, error) {
+	// Open the file for reading
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Create a gzip reader
+	gzipReader, err := gzip.NewReader(file)
+	if err != nil {
+		return nil, err
+	}
+	defer gzipReader.Close()
+
+	// Create a decoder
+	decoder := gob.NewDecoder(gzipReader)
+	// Decode and read the deck object from the file
+	var deck Deck
+	err = decoder.Decode(&deck)
+	if err != nil {
+		return nil, err
+	}
+	return &deck, nil
+}
+
+// Helper Functions
+
+// Parse string for title and ID by splitting string with " of "
+func parseTitle(title string) (string, string) {
+	parts := strings.Split(title, " of ")
+	return parts[0], parts[1]
 }
