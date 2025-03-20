7fcaca05ff5084007cb5ad4c3833c5bb7a00ee8e
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-03-20 13:20:07 -0500

    Refactor: Implement dealing and card display
    This commit refactors the main function to demonstrate the
    deck's dealing functionality and card display.  It creates
    a new deck, shuffles it, and deals a set number of cards.
    The dealt cards are then printed using the Title method.
    Comments related to loading and saving decks are commented out.

--- main.go
+++ main.go
@@ -1,67 +1,73 @@
 package main
 
-import (
-	"fmt"
-
-	"example.com/pkg/deck"
-)
+import "example.com/pkg/deck"
 
 func main() {
-	// creates a new deck
-	deck1, err := deck.LoadGzip("./deck.bin.gz")
+
+	myDeck := deck.NewDeck()
+	myDeck.Shuffle()
+	myCards, err := myDeck.Deal(60)
 	if err != nil {
-		fmt.Println("Error Loadig:", err)
-		deck1 = deck.NewDeck()
-	} else {
-		fmt.Println(deck1.RemainingCards())
+		panic(err)
 	}
-
-	// shuffles the deck
-	deck1.Shuffle()
-	fmt.Println()
-
-	fmt.Println("Warrrrrrrr!!!!!!")
-
-	fmt.Println()
-
-	// picks a card from the deck
-	// prints "Your Card is {Card}"
-	card, err := deck1.PickACard()
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Your Card is", deck1.Title((card)))
+	for _, card := range myCards {
+		println(myDeck.Title(card))
 	}
-	fmt.Println()
+	// // creates a new deck
+	// deck1, err := deck.LoadGzip("./deck.bin.gz")
+	// if err != nil {
+	// 	fmt.Println("Error Loadig:", err)
+	// 	deck1 = deck.NewDeck()
+	// } else {
+	// 	fmt.Println(deck1.RemainingCards())
+	// }
 
-	// shuffles the deck
-	deck1.Shuffle()
-	fmt.Println()
+	// // shuffles the deck
+	// deck1.Shuffle()
+	// fmt.Println()
 
-	// picks a card from the deck
-	// prints "Your Card is {Card}"
-	card, err = deck1.PickACard()
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("My Card is", deck1.Title(card))
-	}
+	// fmt.Println("Warrrrrrrr!!!!!!")
 
-	_, err = deck1.SaveGzip()
-	if err != nil {
-		fmt.Println(err)
-	}
+	// fmt.Println()
 
-	// iterates over teh remaining cards in the deck
-	// prints "Remaining Cards: {Number}"
-	for deck1.RemainingCards() > 0 {
-		card, err = deck1.PickACard()
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println("My Card", deck1.Title(card), deck1.IdentifyCard(deck1.Title(card)))
-		}
-	}
+	// // picks a card from the deck
+	// // prints "Your Card is {Card}"
+	// card, err := deck1.PickACard()
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// } else {
+	// 	fmt.Println("Your Card is", deck1.Title((card)))
+	// }
+	// fmt.Println()
+
+	// // shuffles the deck
+	// deck1.Shuffle()
+	// fmt.Println()
+
+	// // picks a card from the deck
+	// // prints "Your Card is {Card}"
+	// card, err = deck1.PickACard()
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// } else {
+	// 	fmt.Println("My Card is", deck1.Title(card))
+	// }
+
+	// _, err = deck1.SaveGzip()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// // iterates over teh remaining cards in the deck
+	// // prints "Remaining Cards: {Number}"
+	// for deck1.RemainingCards() > 0 {
+	// 	card, err = deck1.PickACard()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	} else {
+	// 		fmt.Println("My Card", deck1.Title(card), deck1.IdentifyCard(deck1.Title(card)))
+	// 	}
+	// }
 }
 
 // Disable cgo checks for pointer size
